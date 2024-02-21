df04a88151f53d3c477f166ed33ee0f24c8588d4
ikikaidesu <mtv.dcknv@yandex.ru>
2024-02-21 19:09:21 +0300

    Add files

---
+++ SnakeGame/Pixel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphic
+{
+    class Pixel
+    {
+        public int X;
+        public int Y;
+        public ConsoleColor Color;
+        public int Size;
+        public char PixelChar = '█';
+
+        public Pixel(int x, int y, ConsoleColor color, int size)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+            Size = size;
+        }
+        
+        // этот метод отвечает за рисовку пикселя
+        public void Draw()
+        {
+            Console.ForegroundColor = Color;
+            // создаем два цикла для отрисовка размерности элемента
+            for (int x = 0; x < Size; x++)
+            {
+                for (int y = 0; y < Size; y++)
+                {
+                    // передаваемая изначальная координата * множитель + 1 = следующая координата
+                    Console.SetCursorPosition(X * Size + x, Y * Size + y);
+                    Console.Write(PixelChar);
+                }
+            }
+        }
+        // этот метод отвечает за очистку элемента
+        public void Clear()
+        {
+            Console.ForegroundColor = Color;
+            for (int x = 0; x < Size; x++)
+            {
+                for (int y = 0; y < Size; y++)
+                {
+                    Console.SetCursorPosition(X*Size+x, Y*Size+y);
+                    Console.Write(" ");
+                }
+            }
+        }
+
+    }
+}
